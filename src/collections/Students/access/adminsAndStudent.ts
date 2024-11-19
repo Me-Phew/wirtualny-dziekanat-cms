c98d91f2cddbf6d472a3a8a1f5850159a61b63ff
Me-Phew <basmateusz@wp.pl>
2024-11-19 14:34:04 +0100

    fix: typescript errors

--- src/collections/Students/access/adminsAndStudent.ts
+++ src/collections/Students/access/adminsAndStudent.ts
@@ -1,6 +1,6 @@
 import type { Access } from 'payload/types';
 
-import { checkRole } from '../checkRole';
+import { checkRole } from '../../../access/checkRole';
 
 const adminsAndUser: Access = ({ req: { user } }) => {
   if (user) {
